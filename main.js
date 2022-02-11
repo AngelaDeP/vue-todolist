c6c7f4b6a718cc4dedc5f18e5bc02a7ab0c13254
Angela De Pasquale <angeladepasquale_@libero.it>
2022-02-11 16:41:38 +0100

    Inserimento in lista delle tasks

---
+++ main.js
@@ -0,0 +1,23 @@
+const app = new Vue ({
+
+    el: '#app',
+    data: {
+
+        newtask: '',
+        tasks: [],
+
+        todo: {
+            todoTask: 'newtask',
+            done: false,
+        }
+
+    },
+    methods: {
+
+        addTask() {
+            this.tasks.push(this.newtask);
+            this.newtask = '';
+        }
+
+    }
+})
